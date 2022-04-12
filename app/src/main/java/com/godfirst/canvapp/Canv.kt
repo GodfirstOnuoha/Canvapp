6f7d126e0a0d3bbbd6bdb81f2efcc94e82f2f833
GodfirstOnuoha <chibuzoonuoha57@gmail.com>
2022-04-12 19:58:16 +0100

    implemented drawing on canvas

--- app/src/main/java/com/godfirst/canvapp/Canv.kt
+++ app/src/main/java/com/godfirst/canvapp/Canv.kt
@@ -1,18 +1,29 @@
 package com.godfirst.canvapp
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
 
 class Canv(context: Context) : View(context) {
     private lateinit var firstFrame: Rect
     private lateinit var secondFrame: Rect
     private lateinit var newCanvas: Canvas
     private lateinit var newBitmap: Bitmap
+
+    private var currentX = 0f
+    private var currentY = 0f
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var path = Path()
+    private val drawing  = Path()
+    private val currentPath = Path()
+
     private val paint = Paint().apply {
         color = ResourcesCompat.getColor(resources, R.color.black, null)
         isAntiAlias = true
@@ -41,6 +52,49 @@ class Canv(context: Context) : View(context) {
         canvas?.drawBitmap(newBitmap, 0f, 0f, null)
         canvas?.drawRect(firstFrame, paint)
         canvas?.drawRect(secondFrame, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event != null) {
+            motionTouchEventX = event.x
+            motionTouchEventY = event.y
+        }
+
+
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+        }
+
+        return true
+    }
+
+    private fun touchUp() {
+        drawing.addPath(currentPath)
+        currentPath.reset()
+    }
+
+    private fun touchMove() {
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+
+            newCanvas.drawPath(path, paint)
+        }
+
+        invalidate()
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX,motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
     }
 
 }
